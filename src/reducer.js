82b5a4ec1d3fa6dd58da7d13b8d4eb8676d919c6
MissDine <enidnyatichi1212@gmail.com>
2022-08-07 22:12:04 +0300

    Feature edit

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,29 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch(action.type) {
+    case "counter/increment":
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action)
+  render();
+}
+
+function render() {
+  document.getElementById('container').innerText = state.count
+}
+
+dispatch({type: "@@INIT"})
+
+// integrate dispatch with a user event
+let button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+  dispatch({type: "counter/increment"})
+})
